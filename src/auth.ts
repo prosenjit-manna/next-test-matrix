52bb129a64e86184aca0a6cb85f16fe40f1cf3d2
Prosenjit Manna <prosenjit@itobuz.com>
2025-04-28 12:51:43 +0530

    mongo db added

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import client from "./app/lib/db"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: MongoDBAdapter(client),
   providers: [
     GitHub
   ],
